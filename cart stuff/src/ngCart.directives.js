40c5008e86177e48e88b2ea9d324d4efbce21f46
Donald R King, II <donrking619@gmail.com>
2015-05-12 15:08:02 -0400

    redo

---
+++ cart stuff/src/ngCart.directives.js
@@ -0,0 +1,88 @@
+'use strict';
+
+
+angular.module('ngCart.directives', ['ngCart.fulfilment'])
+
+    .controller('CartController',['$scope', 'ngCart', function($scope, ngCart) {
+        $scope.ngCart = ngCart;
+    }])
+
+    .directive('ngcartAddtocart', ['ngCart', function(ngCart){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {
+                id:'@',
+                name:'@',
+                quantity:'@',
+                quantityMax:'@',
+                price:'@',
+                data:'='
+            },
+            transclude: true,
+            templateUrl: 'cart stuff/template/ngCart/addtocart.html',
+            link:function(scope, element, attrs){
+                scope.attrs = attrs;
+                scope.inCart = function(){
+                    return  ngCart.getItemById(attrs.id);
+                };
+
+                if (scope.inCart()){
+                    scope.q = ngCart.getItemById(attrs.id).getQuantity();
+                } else {
+                    scope.q = parseInt(scope.quantity);
+                }
+
+                scope.qtyOpt =  [];
+                for (var i = 1; i <= scope.quantityMax; i++) {
+                    scope.qtyOpt.push(i);
+                }
+
+            }
+
+        };
+    }])
+
+    .directive('ngcartCart', [function(){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {},
+            templateUrl: 'cart stuff/template/ngCart/cart.html',
+            link:function(scope, element, attrs){
+
+            }
+        };
+    }])
+
+    .directive('ngcartSummary', [function(){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {},
+            transclude: true,
+            templateUrl: 'cart stuff/template/ngCart/summary.html'
+        };
+    }])
+
+    .directive('ngcartCheckout', [function(){
+        return {
+            restrict : 'E',
+            controller : ('CartController', ['$scope', 'ngCart', 'fulfilmentProvider', function($scope, ngCart, fulfilmentProvider) {
+                $scope.ngCart = ngCart;
+
+                $scope.checkout = function () {
+                    fulfilmentProvider.setService($scope.service);
+                    fulfilmentProvider.setSettings($scope.settings);
+                    var promise = fulfilmentProvider.checkout();
+                    console.log(promise);
+                }
+            }]),
+            scope: {
+                service:'@',
+                settings:'='
+            },
+            transclude: true,
+            templateUrl: 'cart stuff/template/ngCart/checkout.html'
+        };
+    }]);
